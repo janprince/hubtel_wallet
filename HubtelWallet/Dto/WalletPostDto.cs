3a4b9d3a59327e4007ca1ef8a862e3fa2f08dde1
Prince <janprince121@gmail.com>
2024-05-12 23:54:40 +0000

    add DTOs and Initial Tests

--- HubtelWallet/Dto/WalletPostDto.cs
+++ HubtelWallet/Dto/WalletPostDto.cs
@@ -1,43 +1,27 @@
 using System.ComponentModel.DataAnnotations;
+using HubtelWallet.Models;
 
-namespace HubtelWallet.Models;
+namespace HubtelWallet.Dto;
 
-public class Wallet
+public class WalletPostDto
 {
-    public int Id { get; set; }
-    
     [Required]
     [MaxLength(100)]
     public string? Name { get; set; }
     
     [Required]
-    public WalletType Type { get; set; }
+    public Wallet.WalletType Type { get; set; }
     
     [Required]
     [MaxLength(17)]
     public string? AccountNumber { get; set; }
     
     [Required]
-    public AccountScheme Scheme { get; set; }
+    public Wallet.AccountScheme Scheme { get; set; }
     
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  // defaults the createdAt to the "NOW"
 
     [MaxLength(10)]
     [Required]
     public string? OwnerPhoneNumber { get; set; }
-
-    public enum WalletType
-    {
-        MobileMoney = 1 ,
-        Card
-    }
-
-    public enum AccountScheme
-    {
-        Visa = 1,
-        Mastercard,
-        Mtn,
-        Vodafone,
-        AirtelTigo
-    }
 }
